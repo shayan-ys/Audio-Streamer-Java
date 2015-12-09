3715f5e40a6267a2056e8f5a2f11a0a37f522c54
shayan-ys <shayan.yousefian@bamilo.com>
2015-12-09 22:59:05 +0330

    graphical progress bar and mode-based play/pause

--- src/streamer/ClientGUI.java
+++ src/streamer/ClientGUI.java
@@ -25,10 +25,14 @@ public class ClientGUI extends javax.swing.JFrame {
     private Thread progress;
     private Thread timer;
     private static int trackLength;
+    private enum modes { play,pause };
+    private modes mode;
     
     public ClientGUI() {
         
         initComponents();
+        mode = modes.pause;
+        jLabel4.setText(timeFormater(trackLength));
 //        trackLength = player.getMetaLength(1236);
 //        Server serv = new Server();
 //        serv.streamMetaData();
@@ -44,14 +48,17 @@ public class ClientGUI extends javax.swing.JFrame {
         (millis/100)%100);
         return mss;
     }
-    public void updateVars(String mode) {
-        updateTime(mode);
-        updateProgress(mode);
+    public void updateVars() {
+        if(mode == modes.play)
+            jButton1.setText("Pause");
+        else jButton1.setText("Play");
+        updateProgress();
+        updateTime();
     }
     
-    public void updateTime(String mode) {
+    public void updateTime() {
         // Playing
-        if(mode == "resume"){
+        if(mode == modes.play){
             timer = new Thread(
                 new Runnable(){
                     public void run(){
@@ -60,7 +67,7 @@ public class ClientGUI extends javax.swing.JFrame {
                                 ms = timerLastAmount + player.getTime();
                                 if(ms>timerMostCurrent) timerMostCurrent = ms;
                                 //ms += timerLastAmount;
-                                jLabel1.setText("time is:"+ timeFormater(ms));
+                                jLabel1.setText(timeFormater(ms));
                             }
                         }catch(Exception e){
                             JOptionPane.showMessageDialog(null, "Error1 displaying time resuming"+ e);
@@ -86,18 +93,19 @@ public class ClientGUI extends javax.swing.JFrame {
             timerMostCurrent = -1;
         }
     }
-    public void updateProgress(String mode) {
+    public void updateProgress() {
         // Playing
         System.out.println("ms="+ ms);
         System.out.println("trlength="+ trackLength);
-        if(mode == "resume"){
+        if(mode == modes.play){
             progress = new Thread(
                 new Runnable(){
                     public void run(){
                         try{
                             while(true){
                                 progressAmount = (int)(ms*100) / trackLength;
-                                jLabel2.setText("progress is:"+ String.format("%02d",progressAmount) +"%");
+                                jLabel2.setText(String.format("%02d",progressAmount) +"%");
+                                jProgressBar1.setValue(progressAmount);
                             }
                         }catch(Exception e){
                             JOptionPane.showMessageDialog(null, "Error1 displaying progress resuming"+ e);
@@ -128,9 +136,11 @@ public class ClientGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -141,49 +151,47 @@ public class ClientGUI extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Pause");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
+        jLabel1.setText("00:00:00");
 
-        jLabel1.setText("jLabel1");
+        jLabel2.setText("0%");
 
-        jLabel2.setText("jLabel2");
+        jLabel3.setText("/");
+
+        jLabel4.setText("00:00:00");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(27, 27, 27)
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jButton1)
-                        .addGap(45, 45, 45)
-                        .addComponent(jButton2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(116, 116, 116)
-                        .addComponent(jLabel1)))
-                .addContainerGap(173, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(60, 60, 60))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 109, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addGap(64, 64, 64)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(46, 46, 46)
+                .addGap(10, 10, 10)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
                     .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addGap(8, 8, 8)
-                .addComponent(jLabel2)
-                .addGap(22, 22, 22)
-                .addComponent(jLabel1)
-                .addContainerGap(163, Short.MAX_VALUE))
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addContainerGap(235, Short.MAX_VALUE))
         );
 
         pack();
@@ -191,15 +199,13 @@ public class ClientGUI extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        updateVars("resume");
-        player.resume();
+        if(mode == modes.play) mode = modes.pause;
+        else mode = modes.play;
+        updateVars();
+        if(mode == modes.play) player.resume();
+        else player.pause();
+        
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        updateVars("save");
-        player.pause();
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -243,8 +249,10 @@ public class ClientGUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JProgressBar jProgressBar1;
     // End of variables declaration//GEN-END:variables
 }
